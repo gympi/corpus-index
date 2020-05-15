b766d53439f5af7b25c043f6acd34adf44f45a0d
gympi <peter.arhipov@gmail.com>
2020-05-15 10:44:53 +0300

    Dashboard works with unix socket

--- run_dashboard.py
+++ run_dashboard.py
@@ -1,8 +1,22 @@
 import tornado
+from tornado.httpserver import HTTPServer
+from tornado.netutil import bind_unix_socket
+from tornado.options import options, define
 
 from dashboard.server import make_app
 
+define('listen_address', group='webserver', default='127.0.0.1', help='Listen address')
+define('listen_port', group='webserver', default=8095, help='Listen port')
+define('unix_socket', group='webserver', default='/tmp/tags-graph.socket', help='Path to unix socket to bind')
+
 if __name__ == "__main__":
     app = make_app()
-    app.listen(8095)
-    tornado.ioloop.IOLoop.current().start()
+
+    if options.unix_socket:
+        server = HTTPServer(app)
+        socket = bind_unix_socket(options.unix_socket)
+        server.add_socket(socket)
+    else:
+        app.listen(options.listen_port, address=options.listen_address)
+
+    tornado.ioloop.IOLoop.instance().start()
